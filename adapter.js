2aec0ac9fb114e4a512baf50f1e9a18d2e78c61b
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2013-05-08 09:24:18 -0700

    Retrieve jQuery from require() to avoid using global $.

--- adapter.js
+++ adapter.js
@@ -1,5 +1,6 @@
 define(function(require, exports, module) {
 
+  var $ = require("jquery");
   var LayoutManager = require("backbone.layoutmanager");
 
   // Configure LayoutManager with Backbone Boilerplate defaults.
